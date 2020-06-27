cd91b734cfc4cc5edb46ebc6d5bf1eda00e6d37a
Daniel Hupp <daniel.h.web@gmail.com>
2020-06-27 18:54:49 -0400

    Adding parser for archive of our own

--- ff_scrape/storybase.py
+++ ff_scrape/storybase.py
@@ -76,7 +76,7 @@ class Story(object):
     _updated: datetime
     _summary: str
     _rating: str
-    _pairings: List[str]
+    _pairings: List[List[str]]
     _warnings: List[str]
     _characters: List[str]
     _raw_index_page: str
@@ -124,67 +124,67 @@ class Story(object):
     def title(self) -> str: return self._title
 
     @title.setter
-    def title(self, title): self._title = title
+    def title(self, title: str): self._title = title
 
     @property
     def published(self) -> datetime: return self._published
 
     @published.setter
-    def published(self, published): self._published = published
+    def published(self, published: datetime): self._published = published
 
     @property
     def updated(self) -> datetime: return self._updated
 
     @updated.setter
-    def updated(self, updated): self._updated = updated
+    def updated(self, updated: datetime): self._updated = updated
 
     @property
     def status(self) -> str: return self._status
 
     @status.setter
-    def status(self, status): self._status = status
+    def status(self, status: str): self._status = status
 
     @property
     def universe(self) -> List[str]: return self._universe
 
-    def add_universe(self, universe) -> None: self._universe.append(universe)
+    def add_universe(self, universe: str) -> None: self._universe.append(universe)
 
     @property
     def summary(self) -> str: return self._summary
 
     @summary.setter
-    def summary(self, summary): self._summary = summary
+    def summary(self, summary: str): self._summary = summary
 
     @property
     def rating(self) -> str: return self._rating
 
     @rating.setter
-    def rating(self, rating): self._rating = rating
+    def rating(self, rating: str): self._rating = rating
 
     @property
     def categories(self) -> List[str]: return self._categories
 
-    def add_category(self, category) -> None: self._categories.append(category)
+    def add_category(self, category: str) -> None: self._categories.append(category)
 
     @property
     def characters(self) -> List[str]: return self._characters
 
-    def add_character(self, character) -> None: self._characters.append(character)
+    def add_character(self, character: str) -> None: self._characters.append(character)
 
     @property
     def warnings(self) -> List[str]: return self._warnings
 
-    def add_warning(self, warning) -> None: self._warnings.append(warning)
+    def add_warning(self, warning: str) -> None: self._warnings.append(warning)
 
     @property
     def genres(self) -> List[str]: return self._genres
 
-    def add_genre(self, genre) -> None: self._genres.append(genre)
+    def add_genre(self, genre: str) -> None: self._genres.append(genre)
 
     @property
     def pairings(self) -> List[str]: return self._pairings
 
-    def add_pairing(self, pairing) -> None: self._pairings.append(pairing)
+    def add_pairing(self, pairing: List[str]) -> None: self._pairings.append(pairing)
 
     @property
     def word_count(self) -> int:
